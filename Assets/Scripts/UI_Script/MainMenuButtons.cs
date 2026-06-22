b551f7b027876d78ed52ef08b1aec3bb436c3d90
wareniji <jennifer.will0212@gmail.com>
2026-06-22 18:21:04 +0200

    MainMenu Work

--- Assets/Scripts/UI_Script/MainMenuButtons.cs
+++ Assets/Scripts/UI_Script/MainMenuButtons.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour {
-    public void GoToScene(string sceneName) {
-        SceneManager.LoadScene(sceneName);
+    public void GoToScene(string Game_Level01) {
+        SceneManager.LoadScene(Game_Level01);
     }
     
     public void QuitApp() {
