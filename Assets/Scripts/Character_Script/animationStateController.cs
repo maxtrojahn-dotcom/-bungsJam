3ecc7690ff665f48573c459e464a034ddfdcf4f9
dearmelodoll <hoaian.nguyen2507@gmail.com>
2026-06-22 19:50:52 +0200

    Running_Controls

--- Assets/Scripts/Character_Script/animationStateController.cs
+++ Assets/Scripts/Character_Script/animationStateController.cs
@@ -4,20 +4,25 @@ public class animationStateController : MonoBehaviour
 {
     Animator animator;
     int isJoggingHash;
+    int isRunningHash;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
         isJoggingHash = Animator.StringToHash("isJogging");
-        
+        isRunningHash = Animator.StringToHash("isRunning");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isRunning = animator.GetBool(isRunningHash);
         bool isJogging = animator.GetBool(isJoggingHash);
         bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
         if (!isJogging && forwardPressed)
         {
             //then isJogging activates
@@ -28,6 +33,18 @@ public class animationStateController : MonoBehaviour
         {
             //then isJogging deactivates
             animator.SetBool(isJoggingHash, false);
+        }
+
+
+        //press shift to run
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
         }
     }
 }
