2c7fd5ba7ed5fb5684bc1bddd79eeb48e509c1f7
wareniji <jennifer.will0212@gmail.com>
2026-06-22 17:52:36 +0200

    UI Clickable Start

--- Assets/Scripts/UI_Script/MainMenuButtons.cs
+++ Assets/Scripts/UI_Script/MainMenuButtons.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuButtons
-{
+public class MainMenuButtons : MonoBehaviour {
+    public void GoToScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
     
+    public void QuitApp() {
+        Application.Quit();
+        Debug.Log("Application has quit.");
+    }
 }
