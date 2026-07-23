fe13e47b63a3a7c16285ea7d53fdbbd98accc5b4
dearmelodoll <hoaian.nguyen2507@gmail.com>
2026-07-24 00:09:31 +0200

    added audio and bgm prefab

--- Assets/Audio Manager/Generated Struct Classes/Clip.cs
+++ Assets/Audio Manager/Generated Struct Classes/Clip.cs
@@ -3,5 +3,14 @@ namespace CarterGames.Assets.AudioManager
     public struct Clip
     {
         public const string thespeckofdustunderthetable_V2 = "the speck of dust under the table_V2-db527c2a-e32f-42f9-9b44-3d0f19112363";
+        public const string alex_jaukspittingoutwaterfunny211792 = "alex_jauk-spitting-out-water-funny-211792-5bb9f143-78ea-404c-836e-31b80584eba4";
+        public const string antipodeanwritertechhouseorionx27sbelt16439 = "antipodeanwriter-tech-house-orionx27s-belt-16439-e924da69-3bb1-436e-ae83-2466116d28c6";
+        public const string arpmediaupbeatupbeatmusic569476 = "arpmedia-upbeat-upbeat-music-569476-3aaab135-94ac-4227-808f-ef179ac20e0d";
+        public const string freesound_community057982_sonidotripasmp345612 = "freesound_community-057982_sonido-tripasmp3-45612-46199b7f-c3a1-4ac6-8f5f-40f24fa9377e";
+        public const string freesound_communitymandrinkingcoffeethenspitsitback83270 = "freesound_community-man-drinking-coffee-then-spits-it-back-83270-3d35b3f9-880a-4432-9f00-f938b4cf15a9";
+        public const string freesound_communitystonesteps6748 = "freesound_community-stone-steps-6748-61be0b37-eca3-44db-84f6-6bd3c4581a70";
+        public const string jarasnatcrowdnoise284490 = "jarasnat-crowd-noise-284490-64bc5148-4ecb-411d-84b7-8a559c409b92";
+        public const string khoamthanhpunchsoundeffect7ehhx2evi5k500092 = "khoamthanh-punch-sound-effect-7ehhx2evi5k-500092-0df0eefb-7dd6-439f-96be-240be9a54568";
+        public const string storegraphicpunch493595 = "storegraphic-punch-493595-84f49221-5a2f-4182-bf2d-34763ead2a58";
     }
 }
