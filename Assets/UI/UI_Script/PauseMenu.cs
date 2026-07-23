6b79f3b9d4298c867092f3abeb9b2bbb5066eb75
maxtrojahn-dotcom <max.trojahn@outlook.de>
2026-07-24 00:52:53 +0200

    restartFix

--- Assets/UI/UI_Script/PauseMenu.cs
+++ Assets/UI/UI_Script/PauseMenu.cs
@@ -29,6 +29,7 @@ public class PauseMenu : MonoBehaviour
 
     public void Restart()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
